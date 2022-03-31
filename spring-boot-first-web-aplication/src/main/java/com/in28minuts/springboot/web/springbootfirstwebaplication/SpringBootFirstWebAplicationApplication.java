595dafd3acabeaa8077557191bc295208e2b466a
Jackson R Wippel <jrwippel@hotmail.com>
2022-03-31 07:38:51 -0300

    Criação do Projeto JAVA Spring Boot

---
+++ spring-boot-first-web-aplication/src/main/java/com/in28minuts/springboot/web/springbootfirstwebaplication/SpringBootFirstWebAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.in28minuts.springboot.web.springbootfirstwebaplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstWebAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstWebAplicationApplication.class, args);
+	}
+
+}
---
+++ spring-boot-first-web-aplication/src/test/java/com/in28minuts/springboot/web/springbootfirstwebaplication/SpringBootFirstWebAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minuts.springboot.web.springbootfirstwebaplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstWebAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
