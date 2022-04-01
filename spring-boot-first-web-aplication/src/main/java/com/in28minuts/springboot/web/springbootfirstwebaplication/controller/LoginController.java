83521feb132d58458564aba6e013d0808f91cbdb
Jackson R Wippel <jrwippel@hotmail.com>
2022-04-01 17:08:39 -0300

    Validation user and password

--- spring-boot-first-web-aplication/src/main/java/com/in28minuts/springboot/web/springbootfirstwebaplication/controller/LoginController.java
+++ spring-boot-first-web-aplication/src/main/java/com/in28minuts/springboot/web/springbootfirstwebaplication/controller/LoginController.java
@@ -1,22 +1,36 @@
 package com.in28minuts.springboot.web.springbootfirstwebaplication.controller;
 
+import com.in28minuts.springboot.web.springbootfirstwebaplication.services.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
-    public String loginMessage(){
-        return "login";
+    @Autowired
+    LoginService loginService = new LoginService();
+
+
+    @RequestMapping(value="/access", method = RequestMethod.GET)
+    public String showLoginPage(ModelMap model){
+        return "access";
     }
 
-    @RequestMapping("/access")
-    public String accessMessage(@RequestParam String name, ModelMap modelMap){
-        modelMap.put("name", name);
-        return "access";
+    @RequestMapping(value = "/access", method = RequestMethod.POST)
+    public String accessMessage(ModelMap modelMap, @RequestParam String name, @RequestParam String password){
+
+        if (loginService.isValidLogin(name, password)){
+            modelMap.put("name", name);
+            modelMap.put("password", password);
+            return "welcome";
+        }else{
+            modelMap.put("errorMessage", "Invalid Credentials");
+            return "access";
+        }
     }
 }
